289e66dc9a101ddf46d2bb96ab7b91f848fae399
zikasghar <58218255+zikasghar@users.noreply.github.com>
2020-06-24 22:58:38 +0100

    Update MovieDataBaseApi.java

--- app/src/main/java/com/zik/popularmoviesapp/utilities/MovieDataBaseApi.java
+++ app/src/main/java/com/zik/popularmoviesapp/utilities/MovieDataBaseApi.java
@@ -15,7 +15,7 @@ public interface MovieDataBaseApi {
     @GET("movie/popular?api_key=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
     Call<JsonObject> getMoviesByPopular(@Query("page") String page);
 
-    @GET("movie/top_rated?api_key=39ed02b340235a03c4238222e370adbd")
+    @GET("movie/top_rated?api_key=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
     Call<JsonObject> getMoviesByRated(@Query("page") String page);
 
 }
