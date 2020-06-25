2e5363f4fa3d63a6efbd592d5c3349e86ef703eb
ziksghar <zikarya@gmail.com>
2020-06-25 13:49:18 +0100

    initial commit

--- app/src/main/java/com/zik/popularmoviesapp/utilities/MovieDataBaseApi.java
+++ app/src/main/java/com/zik/popularmoviesapp/utilities/MovieDataBaseApi.java
@@ -1,6 +1,7 @@
 package com.zik.popularmoviesapp.utilities;
 
 import com.google.gson.JsonObject;
+import com.zik.popularmoviesapp.constants.API;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
@@ -12,10 +13,10 @@ import retrofit2.http.Query;
 
 public interface MovieDataBaseApi {
 
-    @GET("movie/popular?api_key=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
+    @GET("movie/popular?api_key=" + API.KEY)
     Call<JsonObject> getMoviesByPopular(@Query("page") String page);
 
-    @GET("movie/top_rated?api_key=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
+    @GET("movie/top_rated?api_key=" + API.KEY)
     Call<JsonObject> getMoviesByRated(@Query("page") String page);
 
 }
